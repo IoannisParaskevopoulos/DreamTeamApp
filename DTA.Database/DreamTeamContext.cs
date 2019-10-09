1ee6d8f8b5e7ea3327939c0549f4ffc592935b80
matmarios <mat.marios@gmail.com>
2019-10-09 20:02:34 +0300

    First commit

---
+++ DTA.Database/DreamTeamContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTA.Database
+{
+    public class DreamTeamContext : DbContext
+    {
+        public DreamTeamContext():base("name=DreamTeamAppDB")
+        {
+
+        }
+
+        //public DbSet<T> ts { get; set; }
+    }
+}
---
+++ DTA.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DTA.Web.Startup))]
+namespace DTA.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
